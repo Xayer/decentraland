ea0e50a39ffe748abd45cb5dfeab11a925582fc2
Frederik Rabøl <frederik-rm@hotmail.com>
2021-10-31 17:03:32 +0100

    floating cube above players head

--- src/game.ts
+++ src/game.ts
@@ -10,47 +10,49 @@ class RotatorSystem {
 			// get the Transform component of the entity
 			const transform = entity.getComponent(Transform);
 			const material = entity.getComponent(Material);
-			//const material = new Material();
-			// entity.addComponent(material);
 
-			// mutate the rotation
-			// transform.rotate(Vector3.Up(), dt * 25);
-			transform.rotate(Vector3.Left(), dt * 25);
-			transform.rotate(Vector3.Down(), dt * 25);
-
-			// transform.position.z = transform.position.z + dt * 0.5;
-			// transform.position.x = transform.position.x + dt * 0.5;
-
-			// true = forward | false = backwards
 			const goingForward = material.data.direction;
 			const maxHeight = material.data.maxHeight;
+			const minHeight = material.data.minHeight;
 			const speed = material.data.speed;
 			const brightness = material.data.brightness;
+			const canMove = material.data.canMove;
+			const fromColor = material.data.fromColor;
+			const toColor = material.data.toColor;
+			const floatMultiplier = material.data.floatMultiplier;
 
-			if (goingForward) {
-				transform.position.y = transform.position.y + dt * speed;
-			} else {
-				transform.position.y = transform.position.y - dt * speed;
-			}
+			const currentColor = (material.albedoColor as Color3) ?? Color3.Red();
 
-			// if reached top
-			if (transform.position.y >= maxHeight) {
-				material.data.direction = false;
-				material.albedoColor = Color3.Lerp(
-					Color3.Blue(),
-					Color3.Gray(),
-					brightness
-				);
-			}
+			transform.rotate(Vector3.Left(), speed * dt);
+			transform.rotate(Vector3.Down(), speed * dt);
+			transform.rotate(Vector3.One(), speed * dt);
 
-			// if reached bottom
-			if (transform.position.y <= 1) {
-				material.data.direction = true;
-				material.albedoColor = Color3.Lerp(
-					Color3.Green(),
-					Color3.Red(),
-					brightness
-				);
+			if (canMove) {
+				// transform.position.z = transform.position.z + dt * 0.5;
+				// transform.position.x = transform.position.x + dt * 0.5;
+
+				// true = forward | false = backwards
+
+				if (goingForward) {
+					transform.position.y = transform.position.y + dt * floatMultiplier;
+					material.ambientColor = Color3.Lerp(currentColor, toColor, 1);
+				} else {
+					transform.position.y = transform.position.y - dt * floatMultiplier;
+
+					material.ambientColor = Color3.Lerp(currentColor, fromColor, 1);
+				}
+
+				// if reached top
+				if (transform.position.y >= maxHeight) {
+					material.data.direction = false;
+					// material.ambientColor = Color3.Lerp(currentColor, Color3.Green(), 1);
+				}
+
+				// if reached bottom
+				if (transform.position.y <= minHeight) {
+					material.data.direction = true;
+					// material.ambientColor = Color3.Lerp(currentColor, Color3.Red(), 1);
+				}
 			}
 		}
 	}
@@ -61,7 +63,15 @@ engine.addSystem(new RotatorSystem());
 
 /// --- Spawner function ---
 
-function spawnCube(x: number, y: number, z: number) {
+function spawnCube(
+	x: number,
+	y: number,
+	z: number,
+	canMove = true,
+	minHeight = 1,
+	maxHeight = 2,
+	floatMultiplier = 5
+) {
 	// create the entity
 	const cube = new Entity();
 
@@ -75,15 +85,41 @@ function spawnCube(x: number, y: number, z: number) {
 
 	cube.addComponent(material);
 
-	material.emissiveColor = Color3.Green();
-	material.albedoColor = Color3.Lerp(Color3.Red(), Color3.Gray(), 15);
-	material.ambientColor = Color3.Blue();
+	material.emissiveColor = Color3.Purple();
+	// material.albedoColor = Color3.Lerp(Color3.Red(), Color3.Blue(), 15);
 	material.reflectionColor = Color3.Green();
-	material.dirty = true;
+
+	material.data.canMove = canMove;
 	material.data.direction = true;
-	material.data.maxHeight = Math.floor(Math.random() * 10);
-	material.data.speed = Math.floor(Math.random() * 5);
-	material.data.brightness = Math.floor(Math.random() * 10);
+	material.data.minHeight = minHeight;
+	material.data.maxHeight = maxHeight;
+	material.data.speed = 25;
+	material.data.floatMultiplier = floatMultiplier;
+	material.data.brightness = Math.floor(Math.random() * 5);
+	material.data.fromColor = Color3.Red();
+
+	// material.data.fromColor = new Color3(
+	// 	Math.round(Math.random() * 60),
+	// 	Math.round(Math.random() * 60),
+	// 	Math.round(Math.random() * 60)
+	// );
+
+	material.data.toColor = Color3.Green();
+	// material.data.toColor = new Color3(
+	// 	Math.round(Math.random() * 60),
+	// 	Math.round(Math.random() * 60),
+	// 	Math.round(Math.random() * 60)
+	// );
+
+	material.ambientColor = Color3.Blue();
+
+	material.albedoColor = Color3.Lerp(
+		material.data.fromColor,
+		material.data.toColor,
+		5
+	);
+
+	material.alphaTest = 0.5;
 
 	// add the entity to the engine
 	engine.addEntity(cube);
@@ -93,13 +129,19 @@ function spawnCube(x: number, y: number, z: number) {
 
 /// --- Spawn a cube ---
 
-const cube = spawnCube(8, 1, 8);
+const cube = spawnCube(8, 1, 8, false);
+
+const playerCube = spawnCube(0, 2, 0, true, 1.5, 2, 0.5);
+
+playerCube.setParent(Attachable.AVATAR);
+
+const scale = 0.25;
+
+playerCube.getComponent(Transform).scale.setAll(scale);
 
 cube.addComponent(
 	new OnClick(() => {
-		cube.getComponent(Transform).scale.z *= 1.1;
-		cube.getComponent(Transform).scale.x *= 0.9;
-
-		spawnCube(Math.random() * 8 + 1, Math.random() * 8, Math.random() * 8 + 1);
+		const cubeVisible = playerCube.getComponent(Transform).scale.x === scale;
+		playerCube.getComponent(Transform).scale.setAll(cubeVisible ? 0 : scale);
 	})
 );
